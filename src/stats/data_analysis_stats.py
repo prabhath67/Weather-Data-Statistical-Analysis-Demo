4a104b797ac6b6fb19882f6eacb25417b32e6b9b
Prabhat <tprabhath67@gmail.com>
2022-12-23 15:03:41 +0530

    Adding output tables api responses and test results

--- src/stats/data_analysis_stats.py
+++ src/stats/data_analysis_stats.py
@@ -3,8 +3,15 @@ import json
 import os
 import sqlite3
 import logging
+import sys
+from datetime import datetime
 
+import numpy as np
 import pandas as pd
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 conn = sqlite3.connect('wx_yld_data.db')
@@ -16,16 +23,19 @@ def avg_max_temp():
     """
     res_code = 200
     try:
-        main_dir = os.getcwd().rsplit('\\', 1)[0]
+        main_dir = os.getcwd().rsplit('\\', 2)[0]
+        if 'unittest' in sys.modules.keys():
+            main_dir = os.getcwd().rsplit('\\', 1)[0]
         for table_name in os.listdir(main_dir + '\\wx_data\\'):
             if table_name.endswith(".txt"):
                 table_name = table_name.split('.')[0]
                 df = pd.read_sql('SELECT date,temp_max FROM ' + table_name, conn,
                                  parse_dates={"date": {"format": "%Y%m%d"}})
+                df = df.replace(-9999, np.nan)
                 max_temp_df = df.groupby(lambda x: df['date'][x].year)["temp_max"].max()
-                max_temp_df = max_temp_df.drop_duplicates()
+                max_temp_df.to_csv(main_dir + '\\output\\max_temps\\'+ table_name+ '_avg_min_temp.txt',header=False)
                 max_temp_df.to_sql(table_name + '_avg_max_temp', con=conn, if_exists='replace')
-                logging.info(f'loaded avg max temp stats to  {table_name}_avg_max_temp')
+                logger.info(f'loaded avg max temp stats to  {table_name}_avg_max_temp table')
     except Exception as e:
         logging.error(e)
         res_code = 400
@@ -38,16 +48,19 @@ def avg_min_temp():
     """
     res_code = 200
     try:
-        main_dir = os.getcwd().rsplit('\\', 1)[0]
+        main_dir = os.getcwd().rsplit('\\', 2)[0]
+        if 'unittest' in sys.modules.keys():
+            main_dir = os.getcwd().rsplit('\\', 1)[0]
         for table_name in os.listdir(main_dir + '\\wx_data\\'):
             if table_name.endswith(".txt"):
                 table_name = table_name.split('.')[0]
                 df = pd.read_sql('SELECT date,temp_min FROM ' + table_name, conn,
                                  parse_dates={"date": {"format": "%Y%m%d"}})
+                df = df.replace(-9999, np.nan)
                 min_temp_df = df.groupby(lambda x: df['date'][x].year)["temp_min"].min()
-                min_temp_df = min_temp_df.drop_duplicates()
+                min_temp_df.to_csv(main_dir + '\\output\\min_temps\\' + table_name + '_avg_min_temp.txt', header=False)
                 min_temp_df.to_sql(table_name + '_avg_min_temp', con=conn, if_exists='replace')
-                logging.info(f'loaded avg min temp stats to  {table_name}_avg_min_temp')
+                logger.info(f'loaded avg min temp stats to  {table_name}_avg_min_temp table')
     except Exception as e:
         logging.error(e)
         res_code = 400
@@ -60,16 +73,19 @@ def total_accum_prec():
     """
     res_code = 200
     try:
-        main_dir = os.getcwd().rsplit('\\', 1)[0]
+        main_dir = os.getcwd().rsplit('\\', 2)[0]
+        if 'unittest' in sys.modules.keys():
+            main_dir = os.getcwd().rsplit('\\', 1)[0]
         for table_name in os.listdir(main_dir + '\\wx_data\\'):
             if table_name.endswith(".txt"):
                 table_name = table_name.split('.')[0]
                 df = pd.read_sql('SELECT date,precipitation_amount FROM ' + table_name, conn,
                                  parse_dates={"date": {"format": "%Y%m%d"}})
+                df = df.replace(-9999, np.nan)
                 prec_df = df.groupby(lambda x: df['date'][x].year)["precipitation_amount"].sum()
-                prec_df = prec_df.drop_duplicates()
+                prec_df.to_csv(main_dir + '\\output\\total_prec\\' + table_name + '_precipitation_amount.txt', header=False)
                 prec_df.to_sql(table_name + '_precipitation_amount', con=conn, if_exists='replace')
-                logging.info(f'loaded prec total stats to  {table_name}_precipitation_amount')
+                logger.info(f'loaded prec total stats to  {table_name}_precipitation_amount')
     except Exception as e:
         logging.error(e)
         res_code = 400
@@ -78,20 +94,32 @@ def total_accum_prec():
 
 def get_weather_on_date(date, station_id):
     date_string = date.strftime("%Y%m%d")
+    logger.info(f'Fetching weather on date  {date} for station id {station_id}')
     df = pd.read_sql('SELECT * FROM  ' + station_id  + ' where date = '+ date_string, conn)
+    logger.info(json.loads(df.drop(['index'], axis=1).to_json(orient ='records')))
     return json.loads(df.drop(['index'], axis=1).to_json(orient ='records'))
 
 
 def get_yield_on_date(date):
     date_string = date.strftime("%Y")
+    logger.info(f'Fetching weather on date  {date}')
     df = pd.read_sql('SELECT * FROM US_corn_grain_yield where date = ' + date_string, conn)
+    logger.info(json.loads(df.drop(['index'], axis=1).to_json(orient ='records')))
     return json.loads(df.drop(['index'], axis=1).to_json(orient ='records'))
 
 
 def get_weather_stats(station_id):
+    logger.info(f'Fetching weather for station_id  {station_id}')
     df = pd.read_sql('SELECT * FROM ' + station_id,conn)
+    logger.info(json.loads(df.drop(['index'], axis=1).to_json(orient='records')))
     return json.loads(df.drop(['index'], axis=1).to_json(orient ='records'))
 
 
 if __name__ == '__main__':
     avg_min_temp()
+    avg_max_temp()
+    total_accum_prec()
+    date = datetime.strptime('01/01/1987', '%d/%m/%Y').date()
+    get_weather_on_date(date,'USC00110072')
+    get_yield_on_date(date)
+    get_weather_stats('USC00110072')
--- src/stats/data_load.py
+++ src/stats/data_load.py
@@ -3,13 +3,16 @@
 import os
 import sqlite3
 import logging
+import sys
 from datetime import datetime
 
 import pandas as pd
 
-from src.stats.data_analysis_stats import get_weather_on_date
-
 conn = sqlite3.connect('wx_yld_data.db')
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def load_data():
@@ -19,38 +22,46 @@ def load_data():
     """
     start = datetime.now()
     res_code = 200
-    logging.info(f'Start time : {str(start)}')
+    logger.info(f'Start time : {str(start)}')
     try:
-        main_dir = os.getcwd().rsplit('\\', 1)[0]
+        main_dir = os.getcwd().rsplit('\\', 2)[0]
+        if 'unittest' in sys.modules.keys():
+            main_dir = os.getcwd().rsplit('\\', 1)[0]
+        region_records = 0
         for region_file in os.listdir(main_dir + '\\wx_data\\'):
             if region_file.endswith(".txt"):
                 table_name = region_file.split('.')[0]
-                region_data_df = pd.read_csv(main_dir + '\\wx_data\\' +region_file, sep="\t", names=['date', 'temp_max','temp_min','precipitation_amount'],header=None)
+                region_data_df = pd.read_csv(main_dir + '\\wx_data\\' + region_file, sep="\t",
+                                             names=['date', 'temp_max', 'temp_min', 'precipitation_amount'],
+                                             header=None)
                 region_data_df = region_data_df.drop_duplicates()
                 region_data_df.to_sql(table_name, con=conn, if_exists='replace')
-                logging.info(f'No of records inserted for {region_file} is {region_data_df.shape[0]}')
+                region_records = region_records + region_data_df.shape[0]
+                # logger.info(f'No of records inserted for {region_file} is {region_data_df.shape[0]}')
             else:
-                logging.info(f'Not a valid region file {region_file}')
+                logger.info(f'Not a valid region file {region_file}')
                 continue
+        logger.info(f'Total inserted records for all regions is {region_records}')
 
         for yld_file in os.listdir(main_dir + '\\yld_data\\'):
             if yld_file.endswith(".txt"):
                 table_name = yld_file.split('.')[0]
-                yld_data_df = pd.read_csv(main_dir + '\\yld_data\\' + yld_file, sep="\t", names=['date', 'yield'],header=None)
+                yld_data_df = pd.read_csv(main_dir + '\\yld_data\\' + yld_file, sep="\t", names=['date', 'yield'],
+                                          header=None)
                 yld_data_df = yld_data_df.drop_duplicates()
                 yld_data_df.to_sql(table_name, con=conn, if_exists='replace')
-                logging.info(f'No of records inserted for {yld_file} is {yld_data_df.shape[0]}')
+                logger.info(f'No of records inserted for {yld_file} is {yld_data_df.shape[0]}')
                 break
             else:
-                logging.info(f'Not a valid yield file {yld_file}')
+                logger.info(f'Not a valid yield file {yld_file}')
                 continue
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
         res_code = 400
     end = datetime.now()
-    logging.info(f'End time : {str(end)}')
-    logging.info(f'Time taken to load data is {end - start}')
-    return  {'Status': res_code}
+    logger.info(f'End time : {str(end)}')
+    logger.info(f'Time taken to load data is {end - start}')
+    return {'Status': res_code}
 
 
 if __name__ == '__main__':
